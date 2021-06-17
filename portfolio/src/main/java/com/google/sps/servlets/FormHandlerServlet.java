fb05699cff607680c0b2210f1da6bdda46bc152a
Jennifer <jennifer.jimenez@yale.edu>
2021-06-17 17:23:02 +0000

    Store contact info in Datastore

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -1,5 +1,10 @@
 package com.google.sps.servlets;
 
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,11 +22,23 @@ public class FormHandlerServlet extends HttpServlet {
     String company = request.getParameter("company");
     String email = request.getParameter("email");
     String message = request.getParameter("text-input");
-
-    // Print the value so you can see it in the server logs.
-    System.out.println("Name: " + name + "\nCompany: " + company + "\nEmail: " + email + "\nMessage: " + message);
     
+    // Store contact info in datastore
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    KeyFactory keyFactory = datastore.newKeyFactory().setKind("Task");
+    FullEntity taskEntity =
+        Entity.newBuilder(keyFactory.newKey())
+            .set("name", name)
+            .set("company", company)
+            .set("email", email)
+            .set("message", message)
+            .build();
+    datastore.put(taskEntity);
+
+     // Print the value so you can see it in the server logs.
+    System.out.println("Name: " + name + "\nCompany: " + company + "\nEmail: " + email + "\nMessage: " + message);
+
     // Redirect user back to homepage after submit
-    response.sendRedirect("https://jjimenez-sps-summer21.appspot.com/index.html");
+    response.sendRedirect("/index.html");
   }
 }
